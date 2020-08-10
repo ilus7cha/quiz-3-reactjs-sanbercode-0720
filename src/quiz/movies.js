08ffc2b62d48fae9dad4ead77cfb3a2d3ee64b35
anindita-da <anindita.dian09@gmail.com>
2020-08-10 21:20:18 +0700

    delete comment movie.js

--- src/quiz/movies.js
+++ src/quiz/movies.js
@@ -14,30 +14,6 @@ const Movie = ({title, description, year, duration, genre, rating}) => (
     </section>
 )
 
-/*
-CONTOH RESPONSE API getMovies
- [{
-     "id": 251,
-     "created_at": "2020-08-09 05:39:58",
-     "updated_at": "2020-08-09 05:49:04",
-     "title": "Tolong Trainer di cek API nya",
-     "description": "Error API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDITError API EDIT",
-     "year": 1900,
-     "duration": 2,
-     "genre": "action,hen",
-     "rating": 1
- }, {
-     "id": 265,
-     "created_at": "2020-08-09 05:57:23",
-     "updated_at": "2020-08-09 05:57:23",
-     "title": "API get by ID nya apa ya ?",
-     "description": "API get by ID nya apa ya ?",
-     "year": 1000,
-     "duration": null,
-     "genre": "Musik",
-     "rating": null
- }]
- */
 export default function Movies() {
     const[state, setMovies] = useMovieContext()
 
